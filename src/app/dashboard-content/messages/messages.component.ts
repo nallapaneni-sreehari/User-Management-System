b59eae9852888db2c04267a7541f005e5e4d8a59
r151149 <45209359+r151149@users.noreply.github.com>
2021-09-22 14:15:08 +0530

    changes in messages

--- src/app/dashboard-content/messages/messages.component.ts
+++ src/app/dashboard-content/messages/messages.component.ts
@@ -11,7 +11,7 @@ export class MessagesComponent implements OnInit {
   constructor(private service:ConnectionService) { }
 
   public selectedInd: any;
-  public writeMsg=false;
+  public writeMsg:any=false;
   public sendMsg:any={
     to:"",
     msg:"",
@@ -23,40 +23,28 @@ export class MessagesComponent implements OnInit {
   // public allMessages:any=[];
 
   public allMessages:any = [
-    // {from:"Sattar", to:"Sreehari", msg:"Hi how are you ? It's Sattar", date:"09-15-2021 12:30 PM", type:"rec", status:"unread" },
-    // {from:"Pavan",to:"Sreehari", msg:"Hi bro Iam Pavan !", date:"09-15-2021 2:30 PM", type:"rec", status:"read" },
-    // {from:"Clinton", to:"Sreehari",msg:"Clinton Here ?", date:"09-15-2021 1:30 PM", type:"rec", status:"unread" },
-    // {from:"Param",to:"Sreehari", msg:"Paramsivam is waiting for bus ?", date:"09-15-2021 1:30 PM", type:"rec", status:"unread" },
-    // {from:"Surendra",to:"Sreehari", msg:"Surendra went to market ?", date:"09-15-2021 1:30 PM", type:"rec", status:"read" },
-    // {from:"Soumyajit",to:"Sreehari", msg:"Soumyajit is Online ?", date:"09-15-2021 1:30 PM", type:"rec", status:"read" },
+    // clear us when working with API
+    {msgFrom:"Sattar", msgTo:"sreehari", msgText:"Hi how are you ? It's Sattar", msgDate:"09-15-2021 12:30 PM", type:"rec", status:"unread" },
+    {msgFrom:"Pavan",msgTo:"sreehari", msgText:"Hi bro Iam Pavan !", msgDate:"09-15-2021 2:30 PM", type:"rec", status:"read" },
+    {msgFrom:"Clinton", msgTo:"sreehari",msgText:"Clinton Here ?", msgDate:"09-15-2021 1:30 PM", type:"rec", status:"unread" },
+    {msgFrom:"Param",msgTo:"sreehari", msgText:"Paramsivam is waiting for bus ?", msgDate:"09-15-2021 1:30 PM", type:"rec", status:"unread" },
+    {msgFrom:"Surendra",msgTo:"sreehari", msgText:"Surendra went to market ?", msgDate:"09-15-2021 1:30 PM", type:"rec", status:"read" },
+    {msgFrom:"Soumyajit",msgTo:"sreehari", msgText:"Soumyajit is Online ?", msgDate:"09-15-2021 1:30 PM", type:"rec", status:"read" },
 
 
 
-    // {to:"Sattar", from:"Sreehari", msg:"Hi Sattar, how are you ?", date:"09-15-2021 12:40 PM", type:"sent", status:"sent" },
-    // {to:"Pavan",from:"Sreehari", msg:"Hi Pavan !", date:"09-15-2021 2:34 PM", type:"sent", status:"sent" },
-    // {to:"Clinton", from:"Sreehari",msg:"Hi Clinton ?", date:"09-15-2021 1:31 PM", type:"sent", status:"sent" },
-    // {to:"Param",from:"Sreehari", msg:"Paramsivam Hi", date:"09-15-2021 1:32 PM", type:"sent", status:"sent" },
-    // {to:"Surendra",from:"Sreehari", msg:"Surendra Hello", date:"09-15-2021 1:37 PM", type:"sent", status:"sent" },
-    // {to:"Soumyajit",from:"Sreehari", msg:"Soumyajit Hii", date:"09-15-2021 1:34 PM", type:"sent", status:"sent" },
+    {msgTo:"Sattar", msgFrom:"sreehari", msgText:"Hi Sattar, how are you ?", msgDate:"09-15-2021 12:40 PM", type:"sent", status:"sent" },
+    {msgTo:"Pavan",msgFrom:"sreehari", msgText:"Hi Pavan !", msgDate:"09-15-2021 2:34 PM", type:"sent", status:"sent" },
+    {msgTo:"Clinton", msgFrom:"sreehari",msgText:"Hi Clinton ?", msgDate:"09-15-2021 1:31 PM", type:"sent", status:"sent" },
+    {msgTo:"Param",msgFrom:"sreehari", msgText:"Paramsivam Hi", msgDate:"09-15-2021 1:32 PM", type:"sent", status:"sent" },
+    {msgTo:"Surendra",msgFrom:"sreehari", msgText:"Surendra Hello", msgDate:"09-15-2021 1:37 PM", type:"sent", status:"sent" },
+    {msgTo:"Soumyajit",msgFrom:"sreehari", msgText:"Soumyajit Hii", msgDate:"09-15-2021 1:34 PM", type:"sent", status:"sent" },
 
 ]
 
   public messages:any = []
 
-  public sentMessage:any=[
-    // {msg:"Hi bro, How are you doing ure from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney C ?"
-    // ,to:"Sattar", date:"09-10-2021 12:25 PM", status:"sent", type:"sent"},
-
-    // {msg:"Hello bro, Status of project ?",to:"Clinton", date:"09-10-2021 2:32 PM", status:"sent", type:"sent"},
-
-    // {msg:"Hello bro, Status of project ?",to:"Sattar", date:"09-10-2021 12:33 PM", status:"sent", type:"sent"},
-    // {msg:"Hello bro, Status of project ?",to:"Pavan", date:"09-10-2021 1:32 PM", status:"sent", type:"sent"},
-
-    // {msg:"Hi bro, How to apply OD ?",to:"Surendra", date:"09-10-2021 3:03 PM", status:"sent", type:"sent"},
-    // {msg:"Hi bro, Are your leaves accepted ?",to:"Param", date:"09-09-2021 4:04 PM", status:"sent", type:"sent"},
-    // {msg:"Hi bro, What are you learning bro ?",to:"Pavan", date:"09-09-2021 1:25 PM", status:"sent", type:"sent"},
-    // {msg:"Hi bro, Is it ok if we discuss it in meeting ?",to:"Soumyajit", date:"09-10-2021 12:25 PM", status:"sent", type:"sent"},
-  ];
+  public sentMessage:any=[];
 
   public RecMessages:any=[];
 
@@ -64,7 +52,9 @@ export class MessagesComponent implements OnInit {
   
   
   ngOnInit(): void {
-    this.getAllMessages();
+    this.prepareChats();
+    this.getAllMessages(); //remove me when working with API
+
     // this.allMessages = this.messages.map((item:any, i:any) => Object.assign({}, item, this.sentMessage[i]));
     // console.log("Names:::::",this.allMessages);
 
@@ -87,6 +77,7 @@ export class MessagesComponent implements OnInit {
 
   onChatSelect(chat:any,i:any){
     console.log("Chat Selected:::",chat);
+    this.writeMsg = false;
     this.selectedInd=i;
     this.selectedChat=[];
     this.sendMsg.to = chat.name;
@@ -115,7 +106,7 @@ export class MessagesComponent implements OnInit {
       this.sendMsg.errors=true;
     }
     else{
-      let newMsg = {
+      var newMsg = {
         // from:this.service.Ename,
         msgFrom:"sreehari",
         msgTo:this.sendMsg.to,
@@ -144,7 +135,12 @@ export class MessagesComponent implements OnInit {
 
 
     console.log("Sending message to..",this.sendMsg);
-    this.sendMsg=[];
+    this.sendMsg.msg="";
+
+    // let obj = this.messages.indexOf(this.messages.find((o:any) => o.name === newMsg.msgTo));
+    // console.log("Len::::::", obj);
+    this.writeMsg = false;
+    this.selectedInd = this.messages.indexOf(this.messages.find((o:any) => o.name === newMsg.msgTo));
   }
 
   getAllMessages(){
@@ -167,25 +163,6 @@ export class MessagesComponent implements OnInit {
 
       //Dividing Messages based on sent and received by User
       this.prepareChats();
-      // for(let m of this.allMessages){
-      //   if(m.msgFrom === 'sreehari'){
-      //     m.name = m.msgTo;
-      //     this.messages.push(m);
-      //   }
-      //   else if(m.msgTo === 'sreehari'){
-      //     m.name = m.msgFrom;
-      //     this.messages.push(m);
-      //   }
-      // }
-      // console.log("Before::",this.messages);
-
-      // this.messages = [...new Map(this.messages.map((item:any) =>
-      //   [item['name'], item])).values()];
-
-      // console.log("After::",this.messages);
-
-      // console.log("Sent By Sree::",this.sentMessage);
-      // console.log("Rec By Sree::",this.RecMessages);
 
     });
 
